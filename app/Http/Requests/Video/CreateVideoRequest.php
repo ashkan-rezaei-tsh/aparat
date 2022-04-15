17ded434f76dbe3dc023aba24bfef79588984e8f
Ashkan Rezaei <ashkan.rezaei.tsh@gmail.com>
2022-04-15 17:52:14 +0430

    added publishing video id validation

---
+++ app/Http/Requests/Video/CreateVideoRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Video;
+
+use App\Rules\CategoryIdRule;
+use App\Rules\UploadedBannerIdRule;
+use App\Rules\UploadedVideoIdRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateVideoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'video_id'              => ['required', new UploadedVideoIdRule],
+            'title'                 => 'required|string|max:255',
+            'category'              => ['required', new CategoryIdRule(CategoryIdRule::PUBLIC_CATEGORIES)],
+            'info'                  => 'nullable|string',
+            'tags'                  => 'nullable|array',
+            'tags.*'                => 'exists:tags,id',
+            'playlist'              => 'nullable|exists:playlist,id',
+            'channel_category'      => ['nullable', new CategoryIdRule(CategoryIdRule::PRIVATE_CATEGORIES)],
+            'banner'                => ['nullable', new UploadedBannerIdRule],
+            'publish_at'            => 'nullable|date_format:Y-m-d H:i:s|after:now',
+        ];
+    }
+}
