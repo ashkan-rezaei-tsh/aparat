78c106bcf99776827b679dba81d2c8826ebd10ba
Ashkan Rezaei <ashkan.rezaei.tsh@gmail.com>
2022-05-17 18:07:15 +0430

    changed rule validator to use Storage facade system

--- app/Rules/UploadedVideoBannerIdRule.php
+++ app/Rules/UploadedVideoBannerIdRule.php
@@ -3,6 +3,7 @@
 namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Storage;
 
 class UploadedVideoBannerIdRule implements Rule
 {
@@ -25,7 +26,7 @@ class UploadedVideoBannerIdRule implements Rule
      */
     public function passes($attribute, $value)
     {
-        return file_exists(public_path('videos/tmp/' . $value));
+        return Storage::disk('videos')->exists('tmp/' . $value);
     }
 
     /**
